41be2040f2c23bfe1661f357d39e1faecea1c3e4
seungw0n <stevenswjoeng@gmail.com>
2019-06-05 17:50:57 -0400

    Update package.json by JSW

---
+++ models/User.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+var UserSchema = mongoose.Schema({
+    name: { type: String, required: true},
+    email: { type: String, unique: true, required: true},
+    isVerified: {type: Boolean, default: false},
+    dateOfBirth: Date,
+    address: {
+      unit: String,
+      street: String,
+      pc: String,
+      province: String,
+      country: String
+    },
+    phoneNumber: Number,
+    kId: String,
+    //pic:
+});
+
+var User = module.exports = mongoose.model('User', UserSchema);
+
+module.exports.createUser = (newUser, done) => {
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+            newUser.password = hash;
+            newUser.save(done);
+        });
+    });
+};
+
+module.exports.getUserByUsername = (username, done) => {
+    var query = {email: username};
+    User.findOne(query, done);
+};
+
+module.exports.comparePassword = (candidatePassword, hash, done) => {
+    bcrypt.compare(candidatePassword, hash, (err, isMatch) => {
+        if(err) throw err;
+        done(null, isMatch);
+    });
