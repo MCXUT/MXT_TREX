d826664534e297373ea9babf2101114e7eadc3f5
seungw0n <stevenswjoeng@gmail.com>
2019-06-06 10:08:47 -0400

    Not-completed Register

--- app.js
+++ app.js
@@ -3,18 +3,28 @@ const mongo = require("mongodb");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
 const path = require("path");
+const bodyParser = require("body-parser");
 
-const keys = require("./config/keys");
 const app = express();
+const keys = require("./config/keys");
+
+const registerRoutes = require("./routes/register");
 
 app.set("views", path.join(__dirname, "views"));
 app.set('view engine', 'ejs');
 app.engine("html", ejs.renderFile);
 
+// Set bodyparser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.get('/', function(req,res){
-  res.render("mainpage.html")
+  res.render("mainpage.html");
 });
 
-app.listen(3000, function () {
-  console.log("server opened");
+app.use('/auth', registerRoutes);
+
+app.set('port', process.env.PORT || 8080);
+app.listen(app.get('port'), () => {
+    console.log("Server started on port " + app.get('port'));
 });
---
+++ routes/register.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+
+const User = require("../models/User");
+
+router.post("/register", function(req, res) {
+  var newUser = {
+    name: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  };
+
+  if(!(req.body.name && req.body.email && req.body.password && req.body.password2)) {
+      req.flash("error_signup", "Some information is missing");
+      return res.redirect("/");
+  }
+
+  User.getUserByUsername(newUser.email, function(err,user){
+    if(err) throw err;
+    if(user) {
+      req.flash("error_signup", "Email is already taken!");
+      return res.redirect("/");
+    } else {
+      bcrypt.hash(req.body.password2, 10, function (err, hash){
+        if(err) throw err;
+        User.comparePassword(newUser.password, hash, function (err, isMatch) {
+          if(err) throw err;
+          if(!isMatch) {
+            req.flash("error_signup", "Passwords do not match");
+            return res.redirect("/");
+          } else {
+            var user = new User({
+              name: newUser.name,
+              email: newUser.email,
+              password: newUser.password
+            });
+            User.createUser(user, (err, createdUser) => {
+              if(err) throw err;
+              Verification.createToken(req, res, createdUser);
+            });
+          }
+        });
+      });
+    }
+  })
+});
+
+module.exports = router;
