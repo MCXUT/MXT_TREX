ddfc03bb3f3af9333a4a10074df486e4d7cae290
Yona Kim <yona@Yonas-MacBook.local>
2019-06-08 20:44:24 -0400

    yona

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,12 @@
-
 module.exports = {
     mongodb: {
         user: "partnership",
         pass: "mxtrex%23123"
+    },
+    // Trex 것으로 수정해야함
+    facebookClientInfo: {
+      clientID: "306314140252922",
+      clientSecret: "d71b4ba3659e96edc86087d716dad8ae",
+      callback: "/auth/facebook/callback"
     }
 };
