a325e2d050539c2fc9383bd268b03ca8f8379d53
slee <home@slees-MacBook-Air.local>
2019-06-09 22:47:20 -0400

    add edit method on basic info for user profile

---
+++ public/scripts/profileedit.js
@@ -0,0 +1,75 @@
+$(document).ready(() => {
+    // var editbutton = document.getElementById("edit");
+    // var confirmbutton = document.getElementById("confirm");
+    // var cancelbutton = document.getElementById("cancel");
+    var editbuttons = document.querySelectorAll("#edit");
+    var confirmbuttons = document.querySelectorAll("#confirm");
+    var cancelbuttons = document.querySelectorAll("#cancel");
+
+    editbuttons.forEach((editbutton) => {
+        $(editbutton).click((e) => {
+            e.preventDefault();
+
+            var parentDivision = editbutton.parentElement;
+            var parentDivisionClass = parentDivision.className.split(" ");
+            var allButtons = parentDivision.querySelectorAll("button");
+            allButtons.forEach((button) => {
+                $(button).toggleClass("activebutton");
+            });
+
+            if(parentDivisionClass[1] === "basicinfo") {
+                var spans = document.querySelectorAll(".switch");
+                spans.forEach((span) => {
+                    $(span).toggleClass("active");
+                });
+            }
+        });
+    });
+
+
+    confirmbuttons.forEach((confirmbutton) => {
+        $(confirmbutton).click((e) => {
+            var parentDivision = confirmbutton.parentElement;
+            var parentDivisionClass = parentDivision.className.split(" ");
+            var allButtons = parentDivision.querySelectorAll("button");
+            allButtons.forEach((button) => {
+                $(button).toggleClass("activebutton");
+            });
+
+            if(parentDivisionClass[1] === "basicinfo") {
+
+                var basicinfoform = $("#basicinfoform");
+                // console.log($(basicinfoform).serialize());
+                $.ajax({
+                    type: basicinfoform.attr("method"),
+                    url: basicinfoform.attr("action"),
+                    data: basicinfoform.serialize()
+                });
+
+                var spans = document.querySelectorAll(".switch");
+                spans.forEach((span) => {
+                    $(span).toggleClass("active");
+                });
+            }
+        });
+    });
+
+    cancelbuttons.forEach((cancelbutton) => {
+        $(cancelbutton).click((e) => {
+            e.preventDefault();
+            var parentDivision = cancelbutton.parentElement;
+            var parentDivisionClass = parentDivision.className.split(" ");
+            var allButtons = parentDivision.querySelectorAll("button");
+            allButtons.forEach((button) => {
+                $(button).toggleClass("activebutton");
+            });
+
+            if(parentDivisionClass[1] === "basicinfo") {
+                var spans = document.querySelectorAll(".switch");
+                spans.forEach((span) => {
+                    $(span).toggleClass("active");
+                });
+            }
+        });
+    })
+});
