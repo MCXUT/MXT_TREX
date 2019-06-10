6f6d9bb1060f568f4fd557909670a1a82a26af0f
slee <home@slees-MacBook-Air.local>
2019-06-09 22:51:02 -0400

    move profile routes to user_profile.js

--- app.js
+++ app.js
@@ -70,22 +70,7 @@ app.get('/', function(req,res){
 // app.get("/login", (req, res) => {
 //   res.render("login");
 // });
-app.get("/profile", (req, res) => {
-    var info = {
-        gender: "남자",
-        area: "몬트리올",
-        availability: "타지역 업무 가능",
-        job: "직장",
-        major: "회계/금융"
-    }
-    res.render("userprofile", {info: info});
-});
 
-app.post("/profile", (req, res) => {
-    var newinfo = req.body;
-    console.log(newinfo);
-    res.render("userprofile", {info: newinfo});
-});
 
 app.use('/auth', registerRoutes);
 app.use('/auth', loginRoutes);
