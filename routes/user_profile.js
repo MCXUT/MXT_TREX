0d4ee79a70ce558fd13bda49f9243b8d4f97c3b5
slee <home@slees-MacBook-Air.local>
2019-06-11 18:45:11 -0400

    complete profile language edit and separate partner and client login template

--- routes/user_profile.js
+++ routes/user_profile.js
@@ -1,21 +1,35 @@
 const express = require("express");
 const router = express.Router()
 
-app.get("/user_profile", (req, res) => {
+router.get("/user_profile", (req, res) => {
     var info = {
         gender: "남자",
         area: "몬트리올",
         availability: "타지역 업무 가능",
         job: "직장",
         major: "회계/금융"
-    }
-    res.render("userprofile", {info: info});
+    };
+    res.render("userprofile", {info: info, langinfo: {
+        langchoice: [],
+        langproficiency: []
+    }});
 });
 
-app.post("/user_profile", (req, res) => {
+router.post("/user_profile", (req, res) => {
     var newinfo = req.body;
-    console.log(newinfo);
-    res.render("userprofile", {info: newinfo});
+    // console.log(newinfo);
+    res.render("userprofile", {info: newinfo, langinfo: {}});
 });
+
+router.post("/user_profile/language", (req, res) => {
+
+    var newinfo = {
+        langchoice: req.body.langchoice,
+        langproficiency: req.body.langproficiency
+    }
+    console.log(newinfo);
+
+    res.render("userprofile", {info: {}, langinfo: newinfo});
+})
 
 module.exports = router;
