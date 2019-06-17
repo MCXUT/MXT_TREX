e407335530885f71469ef8922feed560174f70bd
bwkim96 <bwkim96@gmail.com>
2019-06-17 19:01:10 -0400

    Connect findpartner page with DB:loads all partners now + title changes.

--- routes/partnerpages.js
+++ routes/partnerpages.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+const Client = require("../models/Client");
+const Partner = require("../models/Partner");
+
+
 router.get("/partner_page", function(req,res){
   res.render("partnerpage");
 });
@@ -10,7 +14,15 @@ router.get("/partner_profile", function(req, res) {
 });
 
 router.get("/find_partner", function(req,res){
-  res.render("findpartner");
+  Partner.find({}, function(err, allPartners) {
+    if (err) {
+      console.log(err);
+      req.flash("error", "파트너 목록을 불러오는데 문제가 발생했습니다. 다시 시도해주세요.");
+      return res.redirect("/");
+    }
+    res.render("findpartner", { allPartners : allPartners });
+  });
+  // res.render("findpartner");
 });
 
 module.exports = router;
