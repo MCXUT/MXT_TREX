5d12ea0c8a0198965bc1ebabf3c66f0eff297b9a
slee <home@slees-MacBook-Air.local>
2019-06-22 09:28:27 -0400

    Partner profile page complete

--- public/scripts/subbar.js
+++ public/scripts/subbar.js
@@ -1,12 +1,14 @@
-window.onscroll = () => {myFunction()};
-var navbar = document.querySelector(".sectionbar");
-var sticky = navbar.offsetTop;
+// $(document).ready(() => {
+    window.onscroll = () => {myFunction()};
+    var navbar = document.querySelector(".sectionbar");
+    var sticky = navbar.offsetTop;
 
-var myFunction = () => {
-    console.log("navbaroffset: " + sticky + ", windowOffset: " + window.pageYOffset);
-    if(window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky");
-    } else {
-        navbar.classList.remove("sticky");
+    var myFunction = () => {
+        if(window.pageYOffset >= sticky) {
+            navbar.classList.add("sticky");
+        } else {
+            navbar.classList.remove("sticky");
+        }
     }
-}
+
+// });
