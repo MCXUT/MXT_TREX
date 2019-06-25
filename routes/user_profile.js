98f55daf27881446c7bb37a030f4dcd70c54377e
bwkim96 <bwkim96@gmail.com>
2019-06-25 04:43:32 -0400

    Add all profile pages to navbar dropdown and add icons

--- routes/user_profile.js
+++ routes/user_profile.js
@@ -15,7 +15,7 @@ const googleMapsClient = require('@google/maps').createClient({
 
 
 
-
+// GET route for user_profile home route
 router.get("/user_profile", (req, res) => {
   if (!req.user) {
     return res.redirect("/");
@@ -31,6 +31,7 @@ router.get("/user_profile", (req, res) => {
 
 
 // GET route for viewing partner profile info
+// 프로필 관리 (파트너)
 router.get("/user_profile/partnerProfile", (req, res) => {
   if (!req.user) {
     res.redirect("/");
@@ -45,6 +46,7 @@ router.get("/user_profile/partnerProfile", (req, res) => {
 
 
 // GET route for viewing client task request info
+// 견적 요청 관리 (클라이언트)
 router.get("/user_profile/task_request_info", (req, res) => {
   if (!req.user) {
     res.redirect("/");
@@ -59,6 +61,7 @@ router.get("/user_profile/task_request_info", (req, res) => {
 
 
 // GET route for viewing partner task reservation info
+// 업무 예약 관리 (파트너)
 router.get("/user_profile/tasks", (req, res) => {
   if (!req.user) {
     res.redirect("/");
@@ -73,6 +76,7 @@ router.get("/user_profile/tasks", (req, res) => {
 
 
 // GET route for viewing client applicants info
+// 지원자 보기 (클라이언트)
 router.get("/user_profile/applicants_info", (req, res) => {
   if (!req.user) {
     res.redirect("/");
@@ -87,6 +91,7 @@ router.get("/user_profile/applicants_info", (req, res) => {
 
 
 // GET route for viewing partner schedule info
+// 일정 관리 (파트너)
 router.get("/user_profile/schedule", (req, res) => {
   if (!req.user) {
     res.redirect("/");
@@ -103,6 +108,7 @@ router.get("/user_profile/schedule", (req, res) => {
 
 
 // GET route for viewing user profile messages
+// 메세지 (파트너, 클라이언트)
 router.get("/user_profile/messages", (req, res) => {
   if (!req.user) {
     res.redirect("/");
@@ -160,6 +166,7 @@ router.get("/user_profile/messages", (req, res) => {
 
 
 // GET route for viewing user profile payment info
+// 정산 계좌 등록 (파트너)
 router.get("/user_profile/payment_info", (req, res) => {
   if (!req.user) {
     res.redirect("/");
@@ -171,6 +178,7 @@ router.get("/user_profile/payment_info", (req, res) => {
 
 
 // GET route for viewing client saved partners
+// 저장된 파트너 (클라이언트)
 router.get("/user_profile/saved_partners", (req, res) => {
   if (!req.user) {
     res.redirect("/");
@@ -187,6 +195,7 @@ router.get("/user_profile/saved_partners", (req, res) => {
 
 
 // GET route for viewing user profile account info
+// 계정 관리 (파트너, 클라이언트)
 router.get("/user_profile/account_info", (req, res) => {
   if (!req.user) {
     res.redirect("/");
@@ -240,7 +249,7 @@ router.get("/user_profile/account_info", (req, res) => {
 
 
 
-
+// 파트너 프로필 수정
 router.get("/user_profile/edit_partner_resume", function(req, res) {
     if (!req.user) {
       res.redirect("/");
@@ -262,6 +271,7 @@ router.get("/user_profile/edit_partner_resume", function(req, res) {
 
 
 // Create a profile for partner
+// 파트너 프로필 생성
 router.post("/create_new_profile", function(req, res) {
     if (req.body.type == "freelancer") {
         var newProfile = new PartnerProfile({
@@ -326,7 +336,7 @@ router.post("/create_new_profile", function(req, res) {
 });
 
 
-
+// 파트너 프로필 전환 (프리랜서 <-> 에이전시)
 router.post("/convert_partnerProfile_type", function(req, res) {
     if (req.user.type === "p") {
         PartnerProfile.findById(req.user.partnerProfile, function(err, foundProfile) {
