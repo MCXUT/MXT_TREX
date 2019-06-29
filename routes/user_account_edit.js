0ae28301846ec874f999925fcc84c788140e5226
bwkim96 <bwkim96@gmail.com>
2019-06-29 03:19:44 -0400

    Add save partner function

--- routes/user_account_edit.js
+++ routes/user_account_edit.js
@@ -18,11 +18,11 @@ router.post("/user_profile/basicClientInfo", (req, res) => {
       Client.findById(req.user._id, function(err, foundUser) {
         if (err) {
           console.log(err);
-          return res.redirect("/user_profile");
+          return res.redirect("/user_profile/account_info");
         }
         if (!moment(req.body.birthday).isValid()) {
           req.flash("error", "정보를 업데이트 할수 없습니다. 올바른 생일을 입력했는지 확인해주세요.");
-          return res.redirect("/user_profile");
+          return res.redirect("/user_profile/account_info");
         }
         foundUser.dateOfBirth = moment(req.body.birthday).format("MMMM DD, YYYY");
         foundUser.category = req.body.category;
@@ -37,7 +37,7 @@ router.post("/user_profile/basicClientInfo", (req, res) => {
         foundUser.save((err) => {
           if (err) {
             console.log(err);
-            return res.redirect("/user_profile");
+            return res.redirect("/user_profile/account_info");
           }
           console.log("Client Profile Update Successful for " + foundUser.name);
           return res.redirect("/user_profile/account_info");
@@ -51,18 +51,21 @@ router.post("/user_profile/basicClientInfo", (req, res) => {
 router.post("/user_profile/basicPartnerInfo", (req, res) => {
     // Changing profile of a partner
     // Check that the user has input all the fields
-    if (!req.body.streetAddress || !req.body.city || !req.body.country || !req.body.postalCode) {
-        req.flash("error", "정보를 업데이트 할수 없습니다. 모든 정보를 입력했는지 확인해주세요.");
-        return res.redirect("/user_profile");
-    }
+    // if ((!req.body.numberAddress && !req.body.streetAddress) || !req.body.city || !req.body.country || !req.body.postalCode) {
+    //     req.flash("error", "정보를 업데이트 할수 없습니다. 모든 정보를 입력했는지 확인해주세요.");
+    //     return res.redirect("/user_profile/account_info");
+    // }
     
     // Check each field individually if they are valid
-    validateAddress(req.body.streetAddress);
-    validateAddress(req.body.city);
-    validateAddress(req.body.country);
-    validateAddress(req.body.postalCode);
+    // validateAddress(req.body.numberAddress + " " + req.body.streetAddress);
+    // validateAddress(req.body.city);
+    // validateAddress(req.body.state);
+    // validateAddress(req.body.country);
+    if (req.body.postalCode != "") {
+        validateAddress(req.body.postalCode);
+    }
     
-    var fullAddress = req.body.streetAddress + ", " + req.body.city + ", " + req.body.country + ", " + req.body.postalCode;
+    var fullAddress = req.body.numberAddress + " " + req.body.streetAddress + ", " + req.body.city + ", " + req.body.state + ", " + req.body.country + ", " + req.body.postalCode;
       
     // Get coordinates of the new address
     googleMapsClient.geocode({
@@ -71,12 +74,12 @@ router.post("/user_profile/basicPartnerInfo", (req, res) => {
         if (err) {
             console.log(err);
             req.flash("error", "정보를 업데이트 할수 없습니다. 올바른 주소를 입력했는지 확인해주세요.");
-            return res.redirect("/user_profile");
+            return res.redirect("/user_profile/account_info");
         } else {
             // Make sure we are able to get the coordinates of the given address, else throw error
             if (!response.json.results[0]) {
                 req.flash("error", "정보를 업데이트 할수 없습니다. 올바른 주소를 입력했는지 확인해주세요.");
-                return res.redirect("/user_profile");
+                return res.redirect("/user_profile/account_info");
             }
             var newCoordinates = {
                 lat: response.json.results[0].geometry.location.lat,
@@ -86,16 +89,18 @@ router.post("/user_profile/basicPartnerInfo", (req, res) => {
             Partner.findById(req.user._id, function(err, foundUser) {
                 if (err) {
                   console.log(err);
-                  return res.redirect("/user_profile");
+                  return res.redirect("/user_profile/account_info");
                 }
                 if (!moment(req.body.birthday).isValid()) {
                   req.flash("error", "정보를 업데이트 할수 없습니다. 올바른 생일을 입력했는지 확인해주세요.");
-                  return res.redirect("/user_profile");
+                  return res.redirect("/user_profile/account_info");
                 }
                 foundUser.dateOfBirth = moment(req.body.birthday).format("MMMM DD, YYYY");
                 console.log(foundUser.dateOfBirth);
-                foundUser.address.streetAddress = req.body.streetAddress;
+                foundUser.address.streetAddress = req.body.numberAddress + " " + req.body.streetAddress;
+                foundUser.address.detailedAddress = req.body.detailedAddress;
                 foundUser.address.city = req.body.city;
+                foundUser.address.state = req.body.state;
                 foundUser.address.country = req.body.country;
                 foundUser.address.postalCode = req.body.postalCode;
                 foundUser.address.coordinates = newCoordinates;
@@ -105,7 +110,7 @@ router.post("/user_profile/basicPartnerInfo", (req, res) => {
                       if (err) {
                         console.log(err);
                         req.flash("error", "정보를 업데이트 할수 없습니다. 다시 시도해 주세요.");
-                        return res.redirect("/user_profile");
+                        return res.redirect("/user_profile/account_info");
                       }
                       console.log("Partner Profile Update Successful for " + foundUser.name);
                       return res.redirect("/user_profile/account_info");
@@ -132,12 +137,12 @@ function validateAddress(address) {
         if (err) {
             console.log(err);
             req.flash("error", "정보를 업데이트 할수 없습니다. 올바른 주소를 입력했는지 확인해주세요.");
-            return res.redirect("/user_profile");
+            return res.redirect("/user_profile/account_info");
         }
         // Make sure we are able to get the coordinates of the given address, else throw error
         if (!response.json.results[0]) {
             req.flash("error", "정보를 업데이트 할수 없습니다. 올바른 주소를 입력했는지 확인해주세요.");
-            return res.redirect("/user_profile");
+            return res.redirect("/user_profile/account_info");
         }
     });
 }
