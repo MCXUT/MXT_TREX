9dc7aafbdd14fcc797bcd218066519a2d6d31e92
bwkim96 <bwkim96@gmail.com>
2019-07-03 23:14:58 -0400

    In process of passing partner profiles to findpartner.ejs

--- models/Partner.js
+++ models/Partner.js
@@ -113,6 +113,10 @@ var PartnerSchema = mongoose.Schema({
         type: String,
         default: ""
     },
+    // partnerProfile: {
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: "PartnerProfile"
+    // },
     // 가입일자
     registeredDate: {
         type: Date,
