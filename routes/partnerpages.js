3b93d2f067d724a62e6365d4de3fd45dc06a173f
slee <home@slees-MacBook-Air.local>
2019-07-12 18:40:06 -0400

    Add task req form in partner profile page

--- routes/partnerpages.js
+++ routes/partnerpages.js
@@ -6,6 +6,7 @@ const Client = require("../models/Client");
 const Partner = require("../models/Partner");
 const PartnerProfile = require("../models/PartnerProfile");
 const Rating = require("../models/Rating");
+const Service = require("../models/Service");
 const async = require("async");
 
 router.get("/partner_page", function(req, res) {
@@ -14,38 +15,6 @@ router.get("/partner_page", function(req, res) {
 
 // GET route for viewing partner profile page
 router.get("/partner_profile/:id", function(req, res) {
-    // Partner.findById(req.params.id).populate("ratings").exec(function(err, foundUser) {
-    //     var avgRating = 0;
-    //     var allInfos = [];
-    //
-    //     async.each(foundUser.ratings, function(rating, done) {
-    //         avgRating += rating.star;
-    //         Rating.findById(rating.id).populate("byUser").exec(function(err, info) {
-    //             if(info.byUser) {
-    //                 allInfos.push(info);
-    //             } else { // Case where the rated user is deleted
-    //                 console.log(info.id);
-    //                 Rating.findByIdAndRemove(info.id).then(function(deleted) {
-    //                     console.log(deleted);
-    //                 });
-    //             }
-    //             done();
-    //         });
-    //     }, function(err) {
-    //         if(err) throw err;
-    //         else {
-    //             console.log(foundUser.ratings);
-    //             avgRating /= foundUser.ratings.length;
-    //             avgRating = (Math.round(avgRating * 10) / 10).toFixed(1);
-    //             if(avgRating == "NaN") avgRating = 0.0;
-    //             res.render("partnerprofile", {
-    //                 thisPartner: foundUser,
-    //                 averageRating: avgRating,
-    //                 reviews: allInfos
-    //             });
-    //         }
-    //     });
-    // });
     Partner.findById(req.params.id).populate("ratings").exec(function(err, foundPartner) {
         PartnerProfile.findById(foundPartner.partnerProfile, function(err, foundProfile) {
             var avgRating = 0;
@@ -67,23 +36,36 @@ router.get("/partner_profile/:id", function(req, res) {
             }, function(err) {
                 if(err) throw err;
                 else {
-                    console.log(foundPartner.ratings);
                     avgRating /= foundPartner.ratings.length;
                     avgRating = (Math.round(avgRating * 10) / 10).toFixed(1);
                     if(avgRating == "NaN") avgRating = 0.0;
                     if (foundProfile && foundProfile.isVerified) {
-                        res.render("partnerprofile", {
-                            thisPartner: foundPartner,
-                            averageRating: avgRating,
-                            reviews: allInfos,
-                            thisProfile: foundProfile
+                        Service.find({}, (err, allServices) => {
+                            if(err) throw err;
+                            Service.findById(allServices[0].id).populate("questionList").exec((err, detailService) => {
+                                if(err) throw err;
+                                return res.render("partnerprofile", {
+                                    detailService: detailService,
+                                    thisPartner: foundPartner,
+                                    averageRating: avgRating,
+                                    reviews: allInfos,
+                                    thisProfile: foundProfile
+                                });
+                            });
                         });
                     } else {
-                        res.render("partnerprofile", {
-                            thisPartner: foundPartner,
-                            averageRating: avgRating,
-                            reviews: allInfos,
-                            thisProfile: ""
+                        Service.find({}, (err, allServices) => {
+                            if(err) throw err;
+                            Service.findById(allServices[0].id).populate("questionList").exec((err, detailService) => {
+                                if(err) throw err;
+                                return res.render("partnerprofile", {
+                                    detailService: detailService,
+                                    thisPartner: foundPartner,
+                                    averageRating: avgRating,
+                                    reviews: allInfos,
+                                    thisProfile: ""
+                                });
+                            });
                         });
                     }
                 }
