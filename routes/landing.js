205a49848913f6606359b7cbbde6133ce0da9131
slee <home@slees-MacBook-Air.local>
2019-08-19 21:32:46 -0400

    Add file upload system in modal

--- routes/landing.js
+++ routes/landing.js
@@ -7,6 +7,9 @@ const Service = require("../models/Service");
 const Question = require("../models/Question");
 const Task = require("../models/Task");
 
+var fileUpload = require("express-fileupload");
+router.use(fileUpload());
+
 router.get("/", (req, res) => {
     var allInfos = [];
     Service.find({}, (err, allServices) => {
@@ -31,8 +34,9 @@ router.get("/", (req, res) => {
 });
 
 router.post("/task_request", (req, res) => {
-    req.body.numrequired = parseInt(req.body.numrequired.split("")[0]);
-    console.log(typeof req.body.numrequired);
+    console.log(req.body);
+    console.log(req.files);
+    //req.body.numrequired = parseInt(req.body.numrequired.split("")[0]);
     var newTask = new Task(req.body);
     newTask.save((err) => {
         if(err) throw err;
